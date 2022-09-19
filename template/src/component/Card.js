d5d99b76eba612b1e2f07393cb8a1562ba3095d1
Hyunsik Choi <gomgom_@naver.com>
2022-09-20 04:49:24 +0900

    품절 상태 적용
    품절 상태 적용 (프론트만)

--- template/src/component/Card.js
+++ template/src/component/Card.js
@@ -1,36 +1,68 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Card({ name, price, image, id }) {
-  const [moreDetailBtn, setMoreDetailBtn] = useState(false);
+export default function Card({ name, price, image, id, showSoldOutBtn, soldOut }) {
+  const [hover, setHover] = useState(false);
+  const [sold, setSold] = useState(soldOut);
 
   return (
     <div className="card">
+      {/* 마우스 hover 이벤트 추가 */}
       <div
-        className="card-image-container"
-        onMouseOver={() => setMoreDetailBtn(true)}
-        onMouseOut={() => setMoreDetailBtn(false)}
+        className="card-image-container                  "
+        onMouseOver={() => setHover(true)}
+        onMouseOut={() => setHover(false)}
       >
-        <img src={`${image}`} alt={name}></img>
+        {/* public폴더에 img폴더를 넣으면 경로를 간단하게 img/로 시작할 수 있다 */}
+        {sold ? (
+          <img src="img/soldout.png" alt={name}></img>
+        ) : (
+          <img className={hover ? "hover" : ""} src={`${image}`} alt={name}></img>
+        )}
+
+        {/* 상세페이지 클릭 */}
         <Link to={`/card/${id}`}>
-          {moreDetailBtn ? <button className="more-detail">More Detail</button> : null}
+          {hover ? <button className="more-detail">More Detail</button> : null}
         </Link>
       </div>
-      <div className="card-info">
-        <div className="card-name">{name}</div>
-        <div className="card-price">{price}$</div>
-      </div>
-      <div className="x-icon">
-        <i className="bi bi-x"></i>
-      </div>
-      
-      <div className="card-button-container">
-        <Link to={`/purchase/${id}`}>
-          <button>BUY</button>
-        </Link>
-        <button>
-          <i className="bi bi-bag" style={{ fontSize: "1.2rem" }}></i>
-        </button>
+
+      <div className={sold ? "card-info off" : "card-info"} >
+        {/* 카드 이름 가격 */}
+        <div className={sold ? "card-tag off" : "card-tag"}>
+          <div className="card-name">{name}</div>
+          <div className="card-price">{price}$</div>
+        </div>
+  
+        {/* x 아이콘 */}
+        <div className="x-icon">
+          <i className="bi bi-x"></i>
+        </div>
+  
+        {/* soldout 버튼 누르면 checkbox 나타남 */}
+        {showSoldOutBtn ? (
+          <div>
+            <input
+              type="checkbox"
+              id="soldCheck"
+              name="soldCheck"
+              checked={sold}
+              onChange={() => setSold((current) => !current)}
+            />
+            <label htmlFor="soldCheck" style={{ marginLeft: "5px" }}>
+              Sold Out
+            </label>
+          </div>
+        ) : null}
+  
+        {/* buy button과 장바구니 button */}
+        <div className="card-button-container">
+          <Link to={`/purchase/${id}`}>
+            <button disabled={sold}>BUY</button>
+          </Link>
+          <button disabled={sold}>
+            <i className="bi bi-bag" style={{ fontSize: "1.2rem" }}></i>
+          </button>
+        </div>
       </div>
     </div>
   );
--- template/src/component/Main.js
+++ template/src/component/Main.js
@@ -1,13 +1,15 @@
 import Card from "./Card";
 import data from "../db/items.json";
+import { useState } from "react";
 
 export default function Main() {
   console.log(data);
+  const [soldOutClick, setSoldOutClick] = useState(false);
   return (
     <main>
       <div className="main-button-container">
         <button>ADD</button>
-        <button>SOLD OUT</button>
+        <button onClick={() => setSoldOutClick((current) => !current)}>SOLD OUT</button>
       </div>
       <div className="main-container">
         {data.items.map((item) => {
@@ -18,6 +20,8 @@ export default function Main() {
               id={item.id}
               price={item.price}
               image={item.image}
+              showSoldOutBtn={soldOutClick}
+              soldOut={item.soldOut}
             />
           );
         })}
